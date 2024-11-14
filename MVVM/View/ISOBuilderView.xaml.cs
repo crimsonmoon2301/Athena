d875b4978d7ee7c7910f3a0635fc14fa38bc5df2
A Something that is nothing <150836006+crimsonmoon2301@users.noreply.github.com>
2024-11-15 00:37:28 +0200

    Update ISOBuilderView.xaml.cs

--- MVVM/View/ISOBuilderView.xaml.cs
+++ MVVM/View/ISOBuilderView.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.Json;
 
 namespace Galadarbs_IT23033.MVVM.View
 {
@@ -20,9 +22,57 @@ namespace Galadarbs_IT23033.MVVM.View
     /// </summary>
     public partial class ISOBuilderView : UserControl
     {
+        // Ground work for API. I have no idea if it even works.
+        private static readonly HttpClient client = new HttpClient(); // Used to call the API
+
         public ISOBuilderView()
         {
-            InitializeComponent();
+            InitializeComponent();   
+        }
+        public async Task<string> FetchUUPData(string url) // Checks if the API responds. If it does, it returns a string. If not, it returns null.
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        // Incomplete. I will have a migraine with this.
+        public async Task FetchBuildsAsync()
+        {
+            string url = "https://api.uupdump.net/";
+            var response = await client.GetStringAsync(url);
+           /// var buildData = JsonConvert.DeserializeObject<BuildResponse>(response);
+
+           
+        }
+
+        private void ShowAdvancedOptionsCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            AdvancedOptionsPanel.Visibility = Visibility.Visible;
+        }
+
+        private void ShowAdvancedOptionsCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            AdvancedOptionsPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
